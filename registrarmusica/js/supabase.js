4113a92f331903e5e902e84d41c9489154cbddd9
atualrecords <atualrecordsorcamentos@gmail.com>
2026-07-28 14:45:07 -0300

    Add files via upload

--- registrarmusica/js/supabase.js
+++ registrarmusica/js/supabase.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = "https://bdtswwyctsesszfyicoz.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_68U0Spg3sOLRr9ZSiWALAg_jw0sihBF";
 
+window.supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
